1e373805d1804bef38d4e0e0dbfdf741d294ab41
tsuchieryo <tsuchieryo@gmail.com>
2022-12-11 13:06:24 +0900

    敵衝突＆

--- main_tsuchie.rb
+++ main_tsuchie.rb
@@ -5,6 +5,7 @@ require_relative 'enemy'
 require_relative 'enemy2'
 
 font = Font.new(32) # 追加
+fontL = Font.new(64) # 追加
 player_img = Image.load("image/player.png")
 enemy_img = Image.load("image/enemy.png")
 enemy2_img = Image.load("image/enemy2.png")
@@ -12,7 +13,7 @@ enemy2_img = Image.load("image/enemy2.png")
 player = Player.new(100, 100, player_img)
 enemies = []
 enemies2 = []
-1000.times do
+50.times do
   enemies << Enemy.new(rand(0..(640 - 32 - 1)), rand((480 - 32 - 1)), enemy_img)  # 敵描画
   enemies2 << Enemy2.new(rand(0..(640 - 32 - 1)), rand((480 - 32 - 1)), enemy2_img)  # 敵描画
 end
@@ -24,7 +25,23 @@ Window.loop do
   Sprite.draw(enemies)
   Sprite.draw(enemies2)
   Window.draw_font(10, 10, "スコア：#{player.score}", font) # 追加
+  Window.draw_font(500, 10, "HP：#{player.hp}/5", font) # 追加
 
   Sprite.check(player, enemies)
   Sprite.check(player, enemies2)
+
+  # 生存判定
+    if player.dead_flag == 1
+        break
+    end
+  
+end
+
+Window.update
+
+Window.loop do
+    Window.draw_font(Window.width/2-150, Window.height/2 - 50 , "Game Over...", fontL)
+    Window.draw_font(Window.width/2-50, Window.height/2 + 20 , "Continue?", font)
+
+
 end
