6227d2607e9f51917ae3eba1213262a702d3d065
d1828 <d1828@pc5-28.PCROOM>
2022-12-11 16:29:09 +0900

    タイトル修正

--- level.rb
+++ level.rb
@@ -4,16 +4,16 @@ require 'dxruby'
 Window.caption = ("imao-shooting")
 
 #画像の読み込み
-man = Sprite.new(65, 0, Image.load( "image/man.png"))
-ap1 = Sprite.new(65, 380, Image.load("image/ap1.png"))
-ap2 = Sprite.new(248, 380, Image.load("image/ap2.png"))
-ap3 = Sprite.new(427, 380, Image.load("image/ap3.png"))
+mode = Sprite.new(0, 0, Image.load( "image/mode.png"))
+easy = Sprite.new(255, 235, Image.load("image/easy.png"))
+normal = Sprite.new(255, 295, Image.load("image/normal.png"))
+hard = Sprite.new(258, 358, Image.load("image/hard.png"))
 
 #マウスの作成
 mouse = Sprite.new(0, 0, Image.new(10, 10, C_WHITE))
 
 #フォントサイズ
-map_font = Font.new(50); font = Font.new(50)
+map_font = Font.new(35); font = Font.new(35)
 
 #描画ループ
 Window.loop do
@@ -21,28 +21,28 @@ Window.loop do
 	mouse.x, mouse.y = Input.mouse_x, Input.mouse_y
 
     #マップの描画
-	man.draw
+	mode.draw
 
 	#難易度選択の描画
-	ap1.draw; ap2.draw; ap3.draw;
+	easy.draw; normal.draw; hard.draw;
 
     #各難易度にマウスが接触すると
 	case mouse
 
-        when ap1
-            Window.draw_font(190, 265, "click to start", font, z:2)
+        when easy
+            Window.draw_font(220, 420, "click to start", font, z:2)
             #easyモードをクリックした時
             if Input.mouse_push?(M_LBUTTON)
                 require_relative 'main-easy'
             end
-        when ap2
-            Window.draw_font(190, 265, "click to start", font, z:2)
+        when normal
+            Window.draw_font(220, 420, "click to start", font, z:2)
             #normalモードをクリックした時
             if Input.mouse_push?(M_LBUTTON)
                 require_relative 'main-normal'
             end
-        when ap3
-            Window.draw_font(190, 265, "click to start", font, z:2)
+        when hard
+            Window.draw_font(220, 420, "click to start", font, z:2)
             #hardモードをクリックした時
             if Input.mouse_push?(M_LBUTTON)
                 require_relative 'main-hard'
