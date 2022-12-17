97072d15df0663646e4a600124221f44e987b87f
breakahorn <abokado.703@iclod.com>
2022-12-17 11:47:25 +0900

    '対戦形式に変更'

--- player.rb
+++ player.rb
@@ -12,9 +12,39 @@ class Player < Sprite
       super
     end
   
-    def update
-      self.x += 3*Input.x
-      self.y += 3*Input.y
+    def move1
+      if  self.x >= 320 and self.x <= 620 and self.y <= 460 and self.y >= 0
+        self.x += 3*Input.x
+        self.y += 3*Input.y
+      elsif self.x < 320
+        self.x = 320
+      elsif self.x > 620
+        self.x = 620
+      elsif self.y > 460
+        self.y = 460
+      elsif self.y < 0
+        self.y = 0
+      end
+    end
+
+    def move2
+
+      if self.x <= 300 and self.x >= 0 and self.y <= 460 and self.y >= 0
+        self.x += Input.keyDown?(K_D) ? 3 : 0
+        self.x -= Input.keyDown?(K_A) ? 3 : 0
+        self.y += Input.keyDown?(K_S) ? 3 : 0
+        self.y -= Input.keyDown?(K_W) ? 3 : 0
+      elsif self.x > 300
+        self.x = 300
+      elsif self.x < 0
+        self.x = 0
+      elsif self.y > 460
+        self.y = 460
+      elsif self.y < 0
+        self.y = 0
+      end
+
+    
     end
   
     def shot # 相手に衝突したとき
